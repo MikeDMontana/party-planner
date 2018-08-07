78f37fb15cea0b359beb8893702d411c5cc6a604
Mike Dreiling <mdarts@gmail.com>
2018-08-07 14:16:15 -0600

    heroku build script ready

--- app.js
+++ app.js
@@ -18,13 +18,24 @@ require('./passport')(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api/users', users);
+app.use('/api/users', users); //API calls
 
 app.get('/', function(req, res) {
   res.send('hello');
 });
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(_dirname, 'frontend/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(_dirname, 'frontend/build', 'index.html'));
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
