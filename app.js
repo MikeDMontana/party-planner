cc3ad91505f3232d8cb180cdec326f13deab1a7d
Mike Dreiling <mdarts@gmail.com>
2018-08-08 22:54:41 -0600

    remove get root call

--- app.js
+++ app.js
@@ -20,19 +20,19 @@ app.use(bodyParser.json());
 
 app.use('/api/users', users); //API calls
 
-app.get('/', function(req, res) {
-  res.send('hello');
-});
+// app.get('/', function(req, res) {
+//   res.send('hello');
+// });
 
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(_dirname, 'client/build')));
+  app.use(express.static(path.join(_dirname, 'frontend/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(_dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(_dirname, 'frontend/build', 'index.html'));
   });
 }
 
