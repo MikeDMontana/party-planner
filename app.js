55725db360b85a8302f21b234b774b2e1f3e3cf7
Mike Dreiling <mdarts@gmail.com>
2018-08-11 11:34:00 -0600

    added path package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./db');
+const path = require('path');
 
 const users = require('./routes/user');
 
