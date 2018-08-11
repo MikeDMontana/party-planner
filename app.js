1d9b672be42eb341ba9aefd8cd766a5ab5788851
Mike Dreiling <mdarts@gmail.com>
2018-08-11 11:52:48 -0600

    fixed dirname

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(_dirname, 'frontend/build')));
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(_dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
   });
 }
 
